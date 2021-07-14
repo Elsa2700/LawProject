55959dccaf17859deaad040a6ee51c38e87a611a
Chiao <76685877+Elsa2700@users.noreply.github.com>
2021-07-14 12:48:28 +0800

    Add files via upload

---
+++ components/Header/slogan.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Slogan = () => {
+    return (
+        <div className="slogan-frame">
+            <div className="slogan-header">行動法規本</div>
+            <div className="slogan-text-frame">
+                <div className="slogan-text">
+                    <p>你的最佳法規筆記工具</p>
+                    <p>快速找尋您要的法規資訊</p>
+                </div>
+                <i className="darkgrey massive chess knight icon"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Slogan;
