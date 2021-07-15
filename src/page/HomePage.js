b5d9749ea9a9954cd3397def148e3d04b113f8bf
Chiao <76685877+Elsa2700@users.noreply.github.com>
2021-07-16 00:54:43 +0800

    Add files via upload

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -3,12 +3,12 @@ import NavBar from '../components/Navbar/nav'
 import Root from '../components/Navbar/root'
 import Header from '../components/Header/header';
 import "../style/homepage.css"
-import LawMenu from '../components/LawMenu/lawmenu';
+import LawMenu from '../components/LawMenu/LawMenu';
 import { firestore} from '../database/firebase-service'
 import { Link } from 'react-router-dom';
 
 class HomePage extends React.Component {
-    state = {laws:[]};
+    
     render() {
         return (
             <div>
