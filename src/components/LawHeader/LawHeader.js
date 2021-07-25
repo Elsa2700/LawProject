f347f63bcfb7b69ba8270efcb249f64e87324e89
Chiao <76685877+Elsa2700@users.noreply.github.com>
2021-07-25 13:02:47 +0800

    Add files via upload

--- src/components/LawHeader/LawHeader.js
+++ src/components/LawHeader/LawHeader.js
@@ -4,22 +4,22 @@ import '../../style/LawInfo.css'
 
 
 const LawHeader = (props) => {
-    console.log(props.LawInfo)
+    console.log(props)
     return (
             <div className="LawHeader-frame">
                 <div>
                     <header>法規名稱:
-                        <span>{props.LawInfo.LawName}</span>
+                        <span>{props.LawName}</span>
                     </header>
                 </div>
                 <div>
                     <header>修正日期:
-                        <span>{props.LawInfo.LawModifiedDate}</span>
+                        <span>{props.LawModifiedDate}</span>
                     </header>
                 </div>
                 <div>
                     <header>法規類別:
-                        <span>{props.LawInfo.LawCategory}</span>
+                        <span>{props.LawCategory}</span>
                     </header>
                 </div>
             </div>
